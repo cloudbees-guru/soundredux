2722abb7402755d06e4cfd36834c9cc2646423f6
fgibelin <fgibelin@cloudbees.com>
2021-03-03 14:20:02 +0100

    Updated rox-browser to 5.0.0 Updated Rox.Variant to Rox.RoxString

--- client/src/constants/Flags.js
+++ client/src/constants/Flags.js
@@ -10,26 +10,16 @@ const Flags = {
   shuffle: new Rox.Flag(),
   showoverride: new Rox.Flag(),
   repeat: new Rox.Flag(),
-  jsonNba: new Rox.Variant('j1', ['j1', 'j2', 'j3']),
-  startFollowingWord: new Rox.Variant('Follow', ['Follow', 'Start Following', 'Watch Him'])
-};
-
-const roxContainer = {
-  title: new Rox.Configuration("Welcome"),
-  useCDN: new Rox.Configuration(true),
-  networkTimeout: new  Rox.Configuration(100),
-  opacity: new Rox.Configuration(0.7)
+  jsonNba: new Rox.RoxString('j1', ['j1', 'j2', 'j3']),
+  startFollowingWord: new Rox.RoxString('Follow', ['Follow', 'Start Following', 'Watch Him'])
 };
 
 async function initRollout() {
   const options = {}
-
   // Register the flags with Rollout
   Rox.register('', Flags);
-  Rox.register('configs', roxContainer);
   // Setup the Rollout key
   await Rox.setup('5ea2b2fcc5d49a44579da526', options);
-
 }
 
 initRollout().then(function() {
@@ -40,7 +30,6 @@ Rox.setCustomStringProperty('plan', () => UserRepo.getUser().plan);
 Rox.setCustomStringProperty('email', () => UserRepo.getUser().email);
 Rox.setCustomNumberProperty('playlist_count', () => UserRepo.getUser().playlistCount);
 Rox.setCustomStringProperty('soundcloud_id', () => UserRepo.getUser().id);
-
 
 //;
 export default Flags;
